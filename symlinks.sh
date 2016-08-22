5afcf2d9d46d3fe2c4eec029645cc31769e2346e
Justin Restivo <justinjrestivo@gmail.com>
2016-08-22 12:35:43 -0500

    Update symlinks.sh

--- symlinks.sh
+++ symlinks.sh
@@ -80,37 +80,3 @@ ls -l libwrap.so
 }
 
 symLinks 2>&1 | tee /var/tmp/symlinks.log
-
-#function symLinksForced() {
-
-#Alternatively, these are the raw commands
-#if you wanted to force these actions
-
-#remove existing links
-#rm libcrypto.so -f
-#rm libnetsnmp.so -f
-#rm libnetsnmphelpers.so -f
-#rm libperl.so -f
-#rm libnetsnmpagent.so -f
-#rm libnetsnmpmibs.so -f
-#rm libwrap.so -f
-
-#link the libraries
-#ln -s libcrypto.so.10 libcrypto.so
-#ln -s libnetsnmp.so.31 libnetsnmp.so
-#ln -s libnetsnmphelpers.so.31 libnetsnmphelpers.so
-#ln -s perl5/CORE/libperl.so libperl.so
-#ln -s libnetsnmpagent.so.31.0.2 libnetsnmpagent.so
-#ln -s libnetsnmpmibs.so.31 libnetsnmpmibs.so
-#ln -s libwrap.so.0 libwrap.so
-
-#ls -l libcrypto.so
-#ls -l libnetsnmp.so
-#ls -l libnetsnmphelpers.so
-#ls -l libperl.so
-#ls -l libnetsnmpagent.so
-#ls -l libnetsnmpmibs.so
-#ls -l libwrap.so
-}
-
-#symLinksForced 2>&1 | tee /var/tmp/symlinks.log
